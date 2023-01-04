9597e5c3b6b571bb03bdbd696bf1901ed2b320d5
Fausto Oliveira <73948799+faustool-volvo@users.noreply.github.com>
2023-01-04 08:32:47 -0500

    updating build

--- setup.py
+++ setup.py
@@ -1,18 +1,33 @@
+import os
 from setuptools import setup, find_packages
 
+# --------------------
+# Initialization
+# --------------------
+
+VERSION_NUMBER = '0.7.0'
+
+# --------------------
+# Setup
+# --------------------
+
+def read_file(fname):
+    "Read a local file"
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name='mkdocs-childpages-card-grid',
-    version='0.1.0',
-    description='Card grid generator for child pages',
-    long_description='A MkDocs plugin to generate a responsive card grid with all child pages',
-    keywords='mkdocs',
-    url='',
+    name='mkdocs-childpages-card-grid-plugin',
+    version=VERSION_NUMBER,
+    description='Create card grids with links to child pages on your MkDocs site.',
+    long_description=read_file('README.md'),
+    keywords=["mkdocs", "plugin", "python", "child-pages", "card-grid"],
+    url='https://github.com/faustool/mkdocs-childpages-card-grid-plugin',
     author='Fausto Oliveira',
-    author_email='...',
     license='Apache License, Version 2.0',
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     install_requires=[
-        'mkdocs>=1.4.0'
+        'mkdocs>=1.4.0',
+        "mkdocs-material>=8.5.0"
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -22,7 +37,8 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3+'
     ],
-    packages=find_packages(),
+    include_package_data=True,
+    packages=find_packages(exclude=['test']),
     entry_points={
         'mkdocs.plugins': [
             'childpages-card-grid = childpages_card_grid.plugin:ChildPagesCardGridPlugin'
