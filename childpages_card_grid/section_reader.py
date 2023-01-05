a519d03e149dae88feeb311f0b5ef8d02db95c40
Fausto Oliveira <2947769+faustool@users.noreply.github.com>
2023-01-05 13:17:45 -0500

    #4 - executed linters

--- childpages_card_grid/section_reader.py
+++ childpages_card_grid/section_reader.py
@@ -1,36 +1,59 @@
-from mkdocs.plugins import Page
+"""
+Copyright 2023 Fausto Oliveira
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import logging
-log = logging.getLogger("mkdocs.plugins." + __name__)
 
+from typing import Dict, List
+from mkdocs.plugins import Page
+
+LOG = logging.getLogger("mkdocs.plugins." + __name__)
 
 class SectionReader:
+    """
+    Read child sections from a navigation item and populate the nav_map dictionary for use
+    when rendering pages.
+    This function expectes that the first child of every section represents the page itself and
+    the subsequent children represent the actual page children.
+    The scenario below illustrates what this means:
+    Section A - this section
+        Page - the page representing Section A
+        Page - a child page of Section A
+        Section B - a child section of Section A
+            Page - the page representing Section B, therefore a child page of Section *A*
+            Page - a child page of Section B
+        Page - another child page of Section A
+        etc.
+    """
 
-    def read_section(self, section, nav_map: dict[str, list[Page]]()):
+    def read_section(self, section, nav_map: Dict[str, List[Page]]):
         """
-        Read child sections from a navigation item and populate the nav_map dictionary for use when rendering pages.
-
-        This function expectes that the first child of every section represents the page itself and the subsequent children represent the actual page children.
-        The scenario below illustrates what this means:
-        Section A - this section
-            Page - the page representing Section A
-            Page - a child page of Section A
-            Section B - a child section of Section A
-                Page - the page representing Section B, therefore a child page of Section *A* - aha!, didn't expect that, right?
-                Page - a child page of Section B
-            Page - another child page of Section A
-            etc.
+        Read the section
         """
-        if type(section.children[0]) is Page:
-            parent_page = section.children[0] # first child is a page
-            log.debug(f"Parent: {parent_page.title}: {parent_page.file.dest_uri}")
-            nav_map[parent_page.file.dest_uri] = list[Page]() # init the child list for this page
+        if isinstance(section.children[0], Page):
+            parent_page: Page = section.children[0]# first child is a page
+            LOG.debug("Parent: %s: %s", parent_page.title, parent_page.file.dest_uri)
+            nav_map[parent_page.file.dest_uri] = [] # init the child list for this page
             for i in range(1, len(section.children)): # iterate over the subsequent children
                 child_candidate = section.children[i] # whe don't know waht this child is
                 child_page = None
-                if type(child_candidate) is Page: # is it a page? 
+                if isinstance(child_candidate, Page): # is it a page?
                     child_page = child_candidate # the child page is the child itself
-                elif type(child_candidate).__name__ == "Section" and child_candidate.children and type(child_candidate.children[0]) is Page: # is the candidate a section? does it have a child and is the child a page?
-                    child_page = child_candidate.children[0] # the page is the first child of the section
-                log.debug(f"  Child: {child_page.title}: {child_page.file.dest_uri}")
-                nav_map[parent_page.file.dest_uri].append(child_page) # insert the child page to the navigation map
+                # is the candidate a section? does it have a child and is the child a page?
+                elif type(child_candidate).__name__ == "Section" \
+                    and child_candidate.children \
+                    and isinstance(child_candidate.children[0], Page):
+                    # the page is the first child of the section
+                    child_page = child_candidate.children[0]
+                LOG.debug("  Child: %s: %s", child_page.title, child_page.file.dest_uri)
+                # insert the child page to the navigation map
+                nav_map[parent_page.file.dest_uri].append(child_page)
